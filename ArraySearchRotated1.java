790fb920136cbdb079548e6fdc9c53dc038cf5a5
lokeshyadavcs <thatslokesh1@gmail.com>
2021-02-10 00:17:00 +0530

    Array Search in rotated array

---
+++ ArraySearchRotated1.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+
+/**
+ * ArrayRotation1
+ */
+public class ArraySearchRotated1 {
+    
+    static void left(int a[], int n, int d) {
+        int arr[] = new int[n];
+        int i,j,s=n;
+        int b = check(a, d, n);
+      for (j=0,i = b+1; i < n; j++,i++) {
+            arr[j] = a[i];
+
+
+        }
+       for(i=n-1;i>=j;i--,b--){
+           arr[i]=a[b];
+       }
+        for (i = 0; i < s; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    static int check(int a[], int d, int n) {
+        int i;
+        for (i = 0; i < n; i++) {
+            if (a[i] == d) {
+                break;
+            }
+        }
+
+        return i;
+
+    }
+
+    public static void search(int a[],int num){
+        for(int i=0;i<a.length;i++){
+            if(a[i]==num){System.out.println("Number found at :" +i);}
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] a = new int[5];
+        int i;
+        int n = 5;
+        System.out.println("Enter the element");
+        for (i = 0; i < 5; i++) {
+            a[i] = sc.nextInt();
+        }
+        System.out.println("Enter the element to rotate");
+        int d = sc.nextInt();
+        left(a, n, d);
+        System.out.println("Enter the element to find");
+        int num=sc.nextInt();
+        search(a,num);
+
+        
+    }
+}
